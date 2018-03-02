3068e67a498ae1f06df2d0ca748ac6d3d2944510
EthanChing <forfer@msn.com>
2018-03-01 19:01:55 -0800

    Create Wheels.js

---
+++ Wheel/Wheels.js
@@ -0,0 +1,15 @@
+var Wheels = {
+  Wheel : {
+    radius : 10,  
+    distance : 0,
+    year : 0,
+    color : [ 0.0, 0.0, 0.0, 1.0 ]
+  },
+  
+    Rim : {
+    radius : 8,  
+    distance : 0,
+    year : 0,
+    color : [ 1.0, 1.0, 0.0, 1.0 ]
+  },
+  };
